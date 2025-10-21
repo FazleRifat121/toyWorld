2044466ffa847a6a888a2b9287741efd53160ad5
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-10-21 15:49:49 +0600

    fix the mt

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,54 +1,20 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import { Outlet } from "react-router";
 import Navbar from "../components/Nav/Navbar";
 import Footer from "../components/Footer/Footer";
-import ToyWorldIntro from "../components/ToyWorldIntro/ToyWorldIntro.jsx";
-import { gsap } from "gsap";
+import ToyWorldIntro from "../components/ToyWorldIntro/ToyWorldIntro";
 
 function MainLayout() {
 	const [showIntro, setShowIntro] = useState(true);
-	const navbarRef = useRef(null);
-	const outletRef = useRef(null);
-
-	useEffect(() => {
-		if (!showIntro) {
-			// Animate Navbar from top
-			gsap.from(navbarRef.current, {
-				y: -150, // move above the screen
-				opacity: 0,
-				duration: 1,
-				ease: "power3.out",
-			});
-
-			// Animate Outlet content from bottom
-			gsap.from(outletRef.current, {
-				y: 150,
-				opacity: 0,
-				duration: 1,
-				delay: 0.2,
-				ease: "power3.out",
-			});
-		}
-	}, [showIntro]);
 
 	return (
-		<div className="min-h-screen bg-gradient-to-tr from-black via-gray-900 to-black animate-gradient-x text-white relative">
+		<div className="min-h-screen bg-gradient-to-tr from-black via-gray-900 to-black animate-gradient-x text-white">
 			{showIntro ? (
 				<ToyWorldIntro onFinish={() => setShowIntro(false)} />
 			) : (
 				<>
-					<div
-						ref={navbarRef}
-						className="relative w-full z-50 -mt-6" // Ensure Navbar is visible
-					>
-						<Navbar />
-					</div>
-
-					<div ref={outletRef} className="relative z-40 mt-4">
-						<Outlet />
-					</div>
-
-					{/* Footer stays in its own place */}
+					<Navbar />
+					<Outlet />
 					<Footer />
 				</>
 			)}
